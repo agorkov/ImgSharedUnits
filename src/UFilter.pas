0cc7558ff3dacee7daffc053365cfa898122555e
Alexey <gorkoff@gmail.com>
2013-12-19 15:52:42 +0400

    Добавлен фильтр Щарра

--- src/UFilter.pas
+++ src/UFilter.pas
@@ -15,6 +15,7 @@ procedure TruncatedAVGFilter(var GSI: TGreyscaleImage; h, w, d: word);
 procedure LaplaceFilter(var GSI: TGreyscaleImage; AddToOriginal: boolean);
 procedure SobelFilter(var GSI: TGreyscaleImage; AddToOriginal: boolean);
 procedure PrevittFilter(var GSI: TGreyscaleImage; AddToOriginal: boolean);
+procedure SharrFilter(var GSI: TGreyscaleImage; AddToOriginal: boolean);
 
 implementation
 
@@ -23,10 +24,15 @@ uses
 
 const
   LaplaceMask: array [1 .. 3, 1 .. 3] of shortint = ((0, 1, 0), (1, -4, 1), (0, 1, 0));
+
   SobelMaskX: array [1 .. 3, 1 .. 3] of shortint = ((-1, 0, 1), (-2, 0, 2), (-1, 0, 1));
   SobelMaskY: array [1 .. 3, 1 .. 3] of shortint = ((1, 2, 1), (0, 0, 0), (-1, -2, -1));
+
   PrevittMaskX: array [1 .. 3, 1 .. 3] of shortint = ((-1, 0, 1), (-1, 0, 1), (-1, 0, 1));
   PrevittMaskY: array [1 .. 3, 1 .. 3] of shortint = ((1, 1, 1), (0, 0, 0), (-1, -1, -1));
+
+  SharrMaskX: array [1 .. 3, 1 .. 3] of shortint = ((-3, 0, 3), (-10, 0, 10), (-3, 0, 3));
+  SharrMaskY: array [1 .. 3, 1 .. 3] of shortint = ((3, 10, 3), (0, 0, 0), (-3, -10, -3));
 
 function GetPixelValue(const GSI: TGreyscaleImage; i, j: integer): byte;
 begin
@@ -417,7 +423,7 @@ begin
         for fj := -1 to 1 do
           response := response + SobelMaskX[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj) + SobelMaskY[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj);
       if AddToOriginal then
-        response := GSI.i[i, j] - response;
+        response := GSI.i[i, j] + response;
       if response > 255 then
         GSIR.i[i, j] := 255;
       if response < 0 then
@@ -450,7 +456,40 @@ begin
         for fj := -1 to 1 do
           response := response + PrevittMaskX[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj) + PrevittMaskY[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj);
       if AddToOriginal then
-        response := GSI.i[i, j] - response;
+        response := GSI.i[i, j] + response;
+      if response > 255 then
+        GSIR.i[i, j] := 255;
+      if response < 0 then
+        GSIR.i[i, j] := 0;
+      if response in [0 .. 255] then
+        GSIR.i[i, j] := response;
+    end;
+  for i := 1 to GSI.N do
+    for j := 1 to GSI.M do
+      GSI.i[i, j] := GSIR.i[i, j];
+end;
+
+procedure SharrFilter(var GSI: TGreyscaleImage; AddToOriginal: boolean);
+var
+  i, j: integer;
+  fi, fj: integer;
+  response: integer;
+  GSIR: TGreyscaleImage;
+begin
+  UImages.InitGSImg(GSIR, GSI.N, GSI.M);
+  for i := 1 to GSI.N do
+    for j := 1 to GSI.M do
+      GSIR.i[i, j] := GSI.i[i, j];
+
+  for i := 1 to GSI.N do
+    for j := 1 to GSI.M do
+    begin
+      response := 0;
+      for fi := -1 to 1 do
+        for fj := -1 to 1 do
+          response := response + SharrMaskX[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj) + SharrMaskY[fi + 1 + 1, fj + 1 + 1] * GetPixelValue(GSI, i + fi, j + fj);
+      if AddToOriginal then
+        response := GSI.i[i, j] + response;
       if response > 255 then
         GSIR.i[i, j] := 255;
       if response < 0 then
